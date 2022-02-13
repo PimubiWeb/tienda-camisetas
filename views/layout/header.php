48db28cba81ac8de139335124a05be7180bf9357
PabloPC <pimubiweb@gmail.com>
2022-02-13 23:30:51 +0100

    categoria pdo <a>

--- views/layout/header.php
+++ views/layout/header.php
@@ -25,16 +25,18 @@
 						<a href="<?=base_url?>">Inicio</a>
 					</li>
 					
-
-					<?php
+					<li>
+					<?php	
 					//LISTAMOS TODAS LAS CATEGORIAS
 					for($i = 0 ; $i < count($categorias) ; $i++){
-						echo "<li>";
-						echo "<a href='".base_url."categoria/ver&id=".$categorias[$i]['id'].">".$categorias[$i]['nombre']."</a>";
-						echo "</li>";
+						//echo "<a href='".base_url."categoria/ver&id=".$categorias[$i]['id'].">prueba</a>";
+						
 					}
 					?>
+					</li>
 				</ul>
 			</nav>
 
 			<div id="content">
+
+			<a href=""></a>
